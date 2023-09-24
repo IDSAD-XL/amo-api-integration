ec6f4719688236e8028c3f268234efa3144c2433
Alexey <alexey.okorokov.work@gmail.com>
2023-09-24 19:08:51 +0300

    fix

--- src/controllers/leads.controller.js
+++ src/controllers/leads.controller.js
@@ -2,6 +2,10 @@ const catchAsync = require("../utils/catchAsync");
 const leadService = require("../services/leads.service")
 
 const newLead = catchAsync(async (req, res) => {
+  if (req.body?.test === 'test') {
+    res.sendStatus(200)
+    return
+  }
   await leadService.createNewLead(req.body)
   res.sendStatus(200)
 })
