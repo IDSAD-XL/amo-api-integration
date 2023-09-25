dc3483e8b63c06a8afc264335918218bfa6bc4bd
Alexey <alexey.okorokov.work@gmail.com>
2023-09-25 18:24:13 +0300

    fix

--- src/services/leads.service.js
+++ src/services/leads.service.js
@@ -2,7 +2,7 @@ const client = require("../amo-service/client")
 
 const createNewLead = async (entity) => {
   console.log(entity)
-  let { pipeline_id, name, email, phone, price, leadName } = entity
+  let { pipeline_id, name, email, phone, price, leadName, isEntity } = entity
   if (!pipeline_id) {
     pipeline_id = '3865977'
   }
@@ -38,9 +38,15 @@ const createNewLead = async (entity) => {
     price: Number(price),
     pipeline_id: Number(pipeline_id),
     _embedded: {
-      contacts: [{ id: newContact.id }]
+      contacts: [{ id: newContact.id }],
     }
   }
+
+  if (isEntity === true) {
+    leadEntity._embedded.tags = [{ id: 489630 }]
+  }
+
+  console.log(leadEntity)
 
   await client.leads.create([leadEntity])
 }
