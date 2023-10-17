5fbbfe649d06b4555f8829a28869d965c7261803
Alexey <alexey.okorokov.work@gmail.com>
2023-10-17 13:23:54 +0300

    fix

--- src/services/leads.service.js
+++ src/services/leads.service.js
@@ -15,7 +15,7 @@ const addCustomField = (entity, fieldId, value) => {
 }
 
 const createNewLead = async (entity) => {
-  console.log(entity)
+  //console.log(entity)
   let {
     pipeline_id,
     name,
@@ -35,6 +35,7 @@ const createNewLead = async (entity) => {
     utm_term
   } = entity
 
+  //ID доски в AMO
   if (!pipeline_id) {
     pipeline_id = '3865977'
   }
@@ -43,14 +44,17 @@ const createNewLead = async (entity) => {
     price = 0
   }
 
+  //Контакт
   const contactEntity = {
     name: name,
   }
 
+  //Почта
   if (email) {
     addCustomField(contactEntity, 35799, email)
   }
 
+  //Номер телефона
   if (phone) {
     addCustomField(contactEntity, 35797, phone)
   }
@@ -58,15 +62,18 @@ const createNewLead = async (entity) => {
   const newContactArray = await client.contacts.create([contactEntity])
   const newContact = newContactArray[0]
 
+  //Лид
   const leadEntity = {
     name: leadName,
     price: Number(price),
     pipeline_id: Number(pipeline_id),
     _embedded: {
       contacts: [{ id: newContact.id }],
+      tags: [{ id: 484324 }] // SE Online
     }
   }
 
+  //Юрлицо
   if (isEntity === 'yes') {
     if (!leadEntity._embedded.tags) {
       leadEntity._embedded.tags = []
@@ -74,6 +81,7 @@ const createNewLead = async (entity) => {
     leadEntity._embedded.tags.push({ id: 489630 })
   }
 
+  //Кастомные теги
   if (leadTags) {
     if (!leadEntity._embedded.tags) {
       leadEntity._embedded.tags = []
@@ -84,34 +92,42 @@ const createNewLead = async (entity) => {
     }
   }
 
+  //Должность
   if (position) {
-    addCustomField(leadEntity, 831220, position)
+    addCustomField(leadEntity, 831562, position)
   }
 
+  //Сфера деятельности
   if (activityScope) {
-    addCustomField(leadEntity, 831222, activityScope)
+    addCustomField(leadEntity, 831564, activityScope)
   }
 
+  //Курс
   if (course) {
     addCustomField(leadEntity, 682039, course)
   }
 
+  //Метка
   if (utm_source) {
     addCustomField(leadEntity, 37593, utm_source)
   }
 
+  //Метка
   if (utm_campaign) {
     addCustomField(leadEntity, 37597, utm_campaign)
   }
 
+  //Метка
   if (utm_content) {
     addCustomField(leadEntity, 69717, utm_content)
   }
 
+  //Метка
   if (utm_medium) {
     addCustomField(leadEntity, 37595, utm_medium)
   }
 
+  //Метка
   if (utm_term) {
     addCustomField(leadEntity, 69719, utm_term)
   }
